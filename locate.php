b2f01a0eaf74785c7fcef37e402c0778d05fadf3
Mihir Kumar Singh <44443936+mihirkumar02@users.noreply.github.com>
2018-12-03 20:13:15 +0530

    Add files via upload

---
+++ locate.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width" content="initial-scale=1.0">
+    <meta name="description" content="User login and sign up form">
+    <meta name="keywords" content="parking, crowd, malls, smart system">
+    <meta name="author" content="Rachet">
+    <script src="https://maps.googleapis.com/maps/api/js?libraries=places&sensor=false"></script>
+    <link rel="stylesheet" href="css/style.css"/>
+
+    <title>Park It Landing Page | Website</title>
+  </head>
+
+  <body>
+    <header>
+      <div class="container">
+          <div id="branding">
+             <img src="images/parkItLogo.jpg" width="100" height="80"/>
+          </div>
+          <nav>
+            <ul>
+                <li class="current"><a href="site.html">Home</a></li>
+                <li class="current"><a href="about.html">About</a></li>
+            </ul>
+          </nav>
+      </div>
+    </header>
+
+  <section id="showcase">
+    <div class="container">
+      <p>Select your location</p><?php echo $_GET["locater"];?>
+    </div>
+  </section>
+
+  <footer>
+    <p>Park It Smart Parking Solutions, Copyright &copy; 2018</p>
+  </footer>
+</body>
+</html>
